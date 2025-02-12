4aea64ca350898a923970c55fd4785d89e831dce
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-02-12 10:24:25 +0100

    assert that process runs

--- src/test/java/com/camunda/consulting/AppTest.java
+++ src/test/java/com/camunda/consulting/AppTest.java
@@ -1,12 +1,28 @@
 package com.camunda.consulting;
 
+import io.camunda.process.test.api.CamundaAssert;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = { "operate.client.enabled=false","tasklist.client.enabled=false"})
+@SpringBootTest(properties = { "operate.client.enabled=false", "tasklist.client.enabled=false" })
 @CamundaSpringProcessTest
 public class AppTest {
+  @Autowired
+  ZeebeClient zeebeClient;
+
   @Test
-  void contextLoads() {}
+  void shouldExecuteProcess() {
+    ProcessInstanceEvent processInstance = zeebeClient
+        .newCreateInstanceCommand()
+        .bpmnProcessId("AnimalPictureFetcherProcess")
+        .latestVersion()
+        .variable("animalKind", "dog")
+        .send()
+        .join();
+    CamundaAssert.assertThat(processInstance).isCompleted().hasCompletedElements("Fetch dog picture");
+  }
 }
